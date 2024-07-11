2a8505dba5e083b060903169c7ee9fd111274b32
Remar - Eduardo Manosalvas <emanosalvas@remar.com.ec>
2024-07-10 21:22:00 -0500

    Clase 20240710

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PROYECTO2_PROMOCIONES_EMANOSALVAS.Comunes;
+using PROYECTO2_PROMOCIONES_EMANOSALVAS.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PROYECTO2_PROMOCIONES_EMANOSALVAS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        // GET: api/<ClientesController>
+        [HttpGet]
+        public List<Cliente> Get()
+        {
+            return ConexionBD.GetClientes();
+        }
+
+        // GET api/<ClientesController>/5
+        [HttpGet("{cedula}")]
+        public Cliente Get(string cedula)
+        {
+            return ConexionBD.GetCliente(cedula);
+        }
+
+        // POST api/<ClientesController>
+        [HttpPost]
+        public void Post([FromBody] Cliente objCliente)
+        {
+            ConexionBD.PostCliente(objCliente);
+        }
+
+        // PUT api/<ClientesController>/5
+        [HttpPut("{cedula}")]
+        public void Put(string cedula, [FromBody] Cliente objCliente)
+        {
+            ConexionBD.PutCliente(cedula,objCliente);
+        }
+
+        // DELETE api/<ClientesController>/5
+        [HttpDelete("{cedula}")]
+        public void Delete(string cedula)
+        {
+
+        }
+    }
+}
---
+++ Model/Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace PROYECTO2_PROMOCIONES_EMANOSALVAS.Model
+{
+    public class Cliente
+    {
+        public string cedula { get; set; }
+        public string nombre { get; set; }
+        public string estado { get; set; }
+
+    }
+}
